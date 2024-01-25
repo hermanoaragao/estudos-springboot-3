f9e7a6cb1cdc7586899ea22402e27a51d11e538e
Hermano Aragao <haragao@gmail.com>
2024-01-25 18:25:13 -0300

    Projeto de springboot para Portador

---
+++ src/main/java/com/estudos/springboot3/portadorService/PortadorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot3.portadorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortadorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortadorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/springboot3/portadorService/PortadorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springboot3.portadorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortadorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
