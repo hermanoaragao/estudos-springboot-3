cc5d1229f7d31e6881c8cdc09bb8e0d07f0aadf8
Hermano Aragao <haragao@gmail.com>
2024-02-05 07:39:52 -0300

    adicionando dependencia do spring security

--- src/main/java/com/estudos/springboot3/portadorService/controller/token/TokenController.java
+++ src/main/java/com/estudos/springboot3/portadorService/controller/token/TokenController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RequestMapping("/tokens")
 @RestController
+@RequestMapping("/tokens")
 public class TokenController {
 
 	@Value("${url.keycloak}")
