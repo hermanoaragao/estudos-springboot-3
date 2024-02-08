be41f52386f3c9b898e02bf1ed0ddc8dc9701435
Vinicius Soares <vinicius@coffeeandit.com.br>
2024-02-08 13:45:11 -0300

    Primeira revisão do projeto

--- src/main/java/com/estudos/springboot3/portadorService/security/SecurityConfig.java
+++ src/main/java/com/estudos/springboot3/portadorService/security/SecurityConfig.java
@@ -2,20 +2,27 @@ package com.estudos.springboot3.portadorService.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		// desabilitando o spring security
-		http.csrf(csrf -> csrf.disable());
-
+		http
+				.authorizeHttpRequests(authorize -> authorize
+						.anyRequest().authenticated()
+				)
+				.csrf(AbstractHttpConfigurer::disable)
+				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 		return http.build();
 	}
 }
